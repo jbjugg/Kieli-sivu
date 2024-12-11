90cdff1678275eeb15c1f7914529be1f55821a1d
jbjugg <jimi.bjugg@helsinki.fi>
2024-12-11 11:42:50 +0200

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,30 @@
-CREATE TABLE polls (
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT
+);
+CREATE TABLE languages (
+    id SERIAL PRIMARY KEY,
+    language TEXT
+);
+CREATE TABLE exercises (
     id SERIAL PRIMARY KEY,
     topic TEXT,
-    created_at TIMESTAMP
-);
-CREATE TABLE choices (
-    id SERIAL PRIMARY KEY,
-    poll_id INTEGER REFERENCES polls,
-    choice TEXT
+    exercise TEXT,
+    DEADLINE DATE,
+    language_id INTEGER REFERENCES languages,
+    user_id INTEGER REFERENCES users
 );
 CREATE TABLE answers (
     id SERIAL PRIMARY KEY,
-    choice_id INTEGER REFERENCES choices,
-    sent_at TIMESTAMP
+    exercise_id INTEGER REFERENCES exercises,
+    user_id INTEGER REFERENCES users,
+    answer TEXT
+);
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    comment TEXT,
+    exercise_id INTEGER REFERENCES exercises,
+    answer_id INTEGER REFERENCES answers,
+    user_id INTEGER REFERENCES users
 );
